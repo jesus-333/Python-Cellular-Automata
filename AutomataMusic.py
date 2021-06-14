9948030fd6265154c98a9f6af44ef560ea760eca
jesus-333 <albi25.zanca@gmail.com>
2021-06-14 20:05:17 +0200

    Test on sound generation

--- AutomataMusic.py
+++ AutomataMusic.py
@@ -10,7 +10,7 @@ def readFrequnecyFromFile():
     freq_list = []
     
     # Read the files and return the element as a list of string
-    with open('notes_frequency.txt') as f:
+    with open('notes_frequency_2.txt') as f:
         tmp_data = f.readlines()
         
     # Convert the list of string in a list of float
@@ -28,7 +28,9 @@ def rescale(x, a = 0, b = 1):
 class AntsMusic():
     
     def __init__(self, width, height, sound_duration = 1, fs = 44100, channels = 1):
-        self.height, self.width = width, height
+        self.width, self.height = width, height
+        self.fs = fs
+        self.sound_duration = sound_duration
         
         # Read the frequency list
         self.freq_list = readFrequnecyFromFile();
@@ -41,8 +43,8 @@ class AntsMusic():
         self.sound_map = self.createSoundMap(sound_duration = sound_duration, fs = fs)
         
         # 
-        self.p = pyaudio.PyAudio()
-        self.stream = p.open(format = pyaudio.paFloat32, channels = channels, rate = fs, output = True)
+        p = pyaudio.PyAudio()
+        self.stream = p.open(format = pyaudio.paFloat32, channels = 1, rate = self.fs, output = True)
         
     def min_exp(self):
         """
@@ -142,97 +144,104 @@ class AntsMusic():
     
     
     def sound(self, list_of_ants):
-        sine_wave = np.zeros(fs * duration)
+        
+
+        
+        sine_wave = np.zeros(int(self.fs * self.sound_duration))
         for ant in list_of_ants:
             i, j = ant['position']
             
-            sine_wave += (np.sin(2*np.pi*np.arange(fs*duration)* self.sound_map[i, j]/fs)).astype(np.float32)
+            sine_wave += (np.sin(2*np.pi*np.arange(self.fs * self.sound_duration)* self.sound_map[i, j]/self.fs)).astype(np.float32)
         
         # Rescale in 0-1 range
-        sine_wave = rescale(sine_wave)
+        # sine_wave = rescale(sine_wave)
         
         # Reproduce sound
-        stream.write(sine_wave)
+        self.stream.write(sine_wave)
+        # stream.stop_stream()
+        # stream.close()
+        
+        # p.terminate()
 
 
 
 #%%
-import math        #import needed modules
-import pyaudio     #sudo apt-get install python-pyaudioPyAudio = pyaudio.PyAudio     #initialize pyaudio
+# import math        #import needed modules
+# import pyaudio     #sudo apt-get install python-pyaudioPyAudio = pyaudio.PyAudio     #initialize pyaudio
 
 
-volume = 0.5     # range [0.0, 1.0]
-fs = 8000       # sampling rate, Hz, must be integer
-duration = 3.0   # in seconds, may be float
-f1 = 440.0        # sine frequency, Hz, may be float
-f2 = 880.0
+# volume = 0.5     # range [0.0, 1.0]
+# fs = 8000       # sampling rate, Hz, must be integer
+# duration = 3.0   # in seconds, may be float
+# f1 = 440.0        # sine frequency, Hz, may be float
+# f2 = 880.0
 
-# generate samples, note conversion to float32 array
-samples_1 = (np.sin(2*np.pi*np.arange(fs*duration)* f1 /fs)).astype(np.float32)
-samples_2 = (np.sin(2*np.pi*np.arange(fs*duration)* f2 /fs)).astype(np.float32)
-samples_3 = (np.sin(2*np.pi*np.arange(fs*duration)* 220 /fs)).astype(np.float32)
-samples_4 = (np.sin(2*np.pi*np.arange(fs*duration)* 523.25 /fs)).astype(np.float32)
+# # generate samples, note conversion to float32 array
+# samples_1 = (np.sin(2*np.pi*np.arange(fs*duration)* f1 /fs)).astype(np.float32)
+# samples_2 = (np.sin(2*np.pi*np.arange(fs*duration)* f2 /fs)).astype(np.float32)
+# samples_3 = (np.sin(2*np.pi*np.arange(fs*duration)* 220 /fs)).astype(np.float32)
+# samples_4 = (np.sin(2*np.pi*np.arange(fs*duration)* 523.25 /fs)).astype(np.float32)
 
-samples = samples_1 + samples_2 + samples_3 + samples_4
+# samples = samples_1 + samples_2 + samples_3 + samples_4
 
-plt.plot(samples_1)
-plt.plot(samples_2)
-plt.plot(samples)
-plt.xlim([0, 200])
+# plt.plot(samples_1)
+# plt.plot(samples_2)
+# plt.plot(samples)
+# plt.xlim([0, 200])
 
-samples = samples_2
+# samples = samples_2
 
-p = pyaudio.PyAudio()
+# p = pyaudio.PyAudio()
 
-# for paFloat32 sample values must be in range [-1.0, 1.0]
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=fs,
-                output=True)
+# # for paFloat32 sample values must be in range [-1.0, 1.0]
+# stream = p.open(format=pyaudio.paFloat32,
+#                 channels=1,
+#                 rate=fs,
+#                 output=True)
 
-# play. May repeat with different volume values (if done interactively) 
-# stream.write(samples_1)
-# stream.write(samples_2)
-# stream.write(rescale(samples_1))
-# stream.write(rescale(samples_1 + samples_2))
-stream.write(rescale(samples_1 + samples_2 + samples_3))
-stream.write(rescale(samples_1 + samples_2 + samples_3 + samples_4))
+# # play. May repeat with different volume values (if done interactively) 
+# # stream.write(samples_1)
+# # stream.write(samples_2)
+# # stream.write(rescale(samples_1))
+# # stream.write(rescale(samples_1 + samples_2))
+# stream.write(rescale(samples_1 + samples_2 + samples_3))
+# stream.write(rescale(samples_1 + samples_2 + samples_3 + samples_4))
 
-stream.stop_stream()
-stream.close()
+# stream.stop_stream()
+# stream.close()
 
-p.terminate()
-
-
-
-#%%
-
-import math
-import wave
-import struct
+# p.terminate()
 
 
-def synthComplex(freq=[440],coef=[1], datasize=10000, fname="test.wav"):
-    frate = 44100.00  
-    amp=8000.0 
-    sine_list=[]
-    for x in range(datasize):
-        samp = 0
-        for k in range(len(freq)):
-            samp = samp + coef[k] * math.sin(2*math.pi*freq[k]*(x/frate))
-        sine_list.append(samp)
-    wav_file=wave.open(fname,"w")
-    nchannels = 1
-    sampwidth = 2
-    framerate = int(frate)
-    nframes=datasize
-    comptype= "NONE"
-    compname= "not compressed"
-    wav_file.setparams((nchannels, sampwidth, framerate, nframes, comptype, compname))
-    for s in sine_list:
-        wav_file.writeframes(struct.pack('h', int(s*amp/2)))
-    wav_file.close()
+
+# #%%
+
+# import math
+# import wave
+# import struct
+
+
+# def synthComplex(freq=[440],coef=[1], datasize=10000, fname="test.wav"):
+#     frate = 44100.00  
+#     amp=8000.0 
+#     sine_list=[]
+#     for x in range(datasize):
+#         samp = 0
+#         for k in range(len(freq)):
+#             samp = samp + coef[k] * math.sin(2*math.pi*freq[k]*(x/frate))
+#         sine_list.append(samp)
+#     wav_file=wave.open(fname,"w")
+#     nchannels = 1
+#     sampwidth = 2
+#     framerate = int(frate)
+#     nframes=datasize
+#     comptype= "NONE"
+#     compname= "not compressed"
+#     wav_file.setparams((nchannels, sampwidth, framerate, nframes, comptype, compname))
+#     for s in sine_list:
+#         wav_file.writeframes(struct.pack('h', int(s*amp/2)))
+#     wav_file.close()
     
-    return sine_list
+#     return sine_list
 
-a = synthComplex([440,880], [1, 1], 30000, "tone.wav")
+# a = synthComplex([440,880], [1, 1], 30000, "tone.wav")
--- test_Langton Ant.py
+++ test_Langton Ant.py
@@ -2,31 +2,38 @@ import cv2 as cv2
 import numpy as np
 
 from LangtonAnts import LangtonAnts
+from AutomataMusic import AntsMusic
 
 #%%
 
-width = 800
-height = 400
+width = 108
+height = 108
 
-n_ants = 5
+n_ants = 2
 
-n_step = 300000
-# n_step = 0
+# n_step = 300000
+n_step = 0
 
 reproduction = False
 
+fs = 44100
+sound_duration = 0.5
+
 ca_grid = LangtonAnts(width, height, n_ants = n_ants, reproduction = reproduction)
+ca_sound = AntsMusic(width, height, fs = fs, sound_duration = sound_duration)
 
 cv2.namedWindow("Cells", cv2.WINDOW_NORMAL)
 cv2.resizeWindow('Cells', 800, 400)
 
 ca_grid.computeNStep(n_step)
-
+sound_map = ca_sound.sound_map
 
 while(True):
     cv2.imshow("Cells", ca_grid.draw_cells)
     
     ca_grid.update()
+    
+    ca_sound.sound(ca_grid.list_of_ants)
     
     if cv2.waitKey(1) == ord('q'):
         break
